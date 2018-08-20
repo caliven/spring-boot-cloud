fc3ef559dd85b0e9852e56219e8895fb4e71834d
Caliven <liuzhihuil@163.com>
2018-08-20 23:09:49 +0800

    api getway

--- turbine-amqp/src/main/java/com/caliven/turbine/amqp/TurbineAmqpApplication.java
+++ turbine-amqp/src/main/java/com/caliven/turbine/amqp/TurbineAmqpApplication.java
@@ -2,9 +2,11 @@ package com.caliven.turbine.amqp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
 @EnableTurbineStream
+@EnableDiscoveryClient
 @SpringBootApplication
 public class TurbineAmqpApplication {
 
