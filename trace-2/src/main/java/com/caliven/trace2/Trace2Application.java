0af9581aa4ebb9cd235a0cdafa08fe961ed51da7
Caliven <liuzhihuil@163.com>
2018-11-03 15:24:02 +0800

    trace

---
+++ trace-2/src/main/java/com/caliven/trace2/Trace2Application.java
@@ -0,0 +1,32 @@
+package com.caliven.trace1;
+
+import org.apache.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Trace2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Trace2Application.class, args);
+    }
+
+
+    private final Logger logger = Logger.getLogger(getClass());
+
+
+    @RequestMapping(value = "/trace-2", method = RequestMethod.GET)
+    public String trace() {
+        logger.info("===<call trace-2>===");
+        return "Trace";
+    }
+}
