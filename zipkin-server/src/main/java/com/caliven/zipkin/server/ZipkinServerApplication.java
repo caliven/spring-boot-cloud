0910bee229e88d017d8f230ce408f889c31e2cb5
Caliven <liuzhihuil@163.com>
2018-11-04 17:34:39 +0800

    zipkin-server

--- zipkin-server/src/main/java/com/caliven/zipkin/server/ZipkinServerApplication.java
+++ zipkin-server/src/main/java/com/caliven/zipkin/server/ZipkinServerApplication.java
@@ -2,9 +2,12 @@ package com.caliven.zipkin.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import zipkin.server.internal.EnableZipkinServer;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 
-@EnableZipkinServer
+// 以http方式启动
+// @EnableZipkinServer
+// 以 stream 结合 RabbitMQ方式启动服务
+@EnableZipkinStreamServer
 @SpringBootApplication
 public class ZipkinServerApplication {
 
